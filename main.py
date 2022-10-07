91a6e2d0dcb75eab38b1be5615ca9289db254b3c
CHIEH-HUNG CHENG <39375337+chengchiehhung@users.noreply.github.com>
2022-10-07 08:53:39 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,89 @@
+### 2022/10/07 - KCIS AI & Data Science Club ###
+
+#### Topic I ####
+# 語法 (Syntax) ~ 文法 (Grammar)
+# Data Types (資料型別)
+3 # Integer (整數)
+6.6 # Float (浮點數、小數)
+"Jeff" # String (字串)
+'How are you' # String (字串)
+# a = [3, "Ok", [1,2,3,]] # list (列表) 
+# a[1] = "not Ok"
+# print(a)
+# a = (3, "Ok", [1,2,3,])  # Tuple 元組
+# a[1] = "not ok"
+# print(a)
+{"傑夫": "Jeff"} # Dictionary (字典) # key-value pair
+# ... Data types
+
+# Arithmetic Operators (算數運算子)
+# +, -, *. /, // (取整數), % (取餘數)
+# print(5%4)
+
+# Boolean Logic (布林邏輯) => 布林值 (True / False)
+# 3 > 4 False
+# "a" == "a" True
+# Conditional Statement (條件判斷式)
+# 變數 (Variable) => 存東西 之後可以拿來用
+# gender = 3
+
+# if gender == "male":
+#   print("請走右邊")
+
+# Loops (迴圈)
+# for-loop while loop
+
+# for 迭代變數 in 迭代物件: iterable: 可以一個一個數
+# for i in ["milk", "apple", "book"]: # 3 次
+#   print(i)
+
+# for i in range(3): # 3 次 range(3): 數列 0, 1, 2
+#   print(i)
+
+# for i in "How are you?":
+#   print(i)
+
+# while Look
+# a = 5
+# while a > 0:
+#   print("Hi")
+#   a = a - 1
+
+
+#### Topic II ####
+# List (列表)
+# a = ["Jeff", [2, 3, 4], (6, 6)]
+# index => 0
+# for i in a:
+#   for j in i:
+#     print(j)
+
+
+# Dictionary (字典)
+# key-value pairs
+# a = {"name": "Jeff", "age": 25} # iterable
+# for i in a:
+#   print(a[i])
+
+#### Iterable Objects (迭代物件) in Python ####
+
+
+# 在 Python，所有的東西都是 物件(Object): 1. 屬性 (attribute/property) 2. 方法 (methods)
+# 透過 . 來存取 1. 2.
+# c = 0
+# for i in "Jeff":
+#   if i == "e":
+#     c = c + 1
+# print(c)
+
+
+# "Jeff".count("e")
+# "Jeff"
+
+# num = input("Please enter a number: ") # string
+# num = int(num)
+
+# for i in range(num): # 0, 1, 2, ... num-1
+#   print("hi")
+
+
